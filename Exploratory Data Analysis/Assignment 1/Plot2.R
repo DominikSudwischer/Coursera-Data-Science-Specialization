6dbe9a3b4cda3bade7ed76d73d42dff5a33374a9
Dominik Sudwischer <dominik.sudwischer@gmail.com>
2017-10-31 23:19:41 +0100

    Remove some unnecessary code...
    ... from the early phase of my solution.

--- Exploratory Data Analysis/Assignment 1/Plot2.R
+++ Exploratory Data Analysis/Assignment 1/Plot2.R
@@ -2,7 +2,6 @@
 
 # Load libraries
 library(lubridate)
-library(hashmap)
 
 # Load data
 Sys.setlocale("LC_TIME", "English")
@@ -14,10 +13,6 @@ df <- read.table("household_power_consumption.txt", sep = ";", header = TRUE,
 # Process data
 df <- df[as.character(df$Date) %in% c("1/2/2007", "2/2/2007"), ]
 df$DT <- as.POSIXct(paste(df$Date, df$Time), format = "%d/%m/%Y %H:%M:%S")
-df$Time <- as.POSIXct(df$Time, format = "%H:%M:%S")
-df$Date <- as.Date.factor(df$Date, "%d/%m/%Y")
-wd <- hashmap(c(0, 1, 2, 3, 4, 5, 6), c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
-df$weekday <- wd[[wday(df$DT)]]
 
 # Plot 2
 png(filename = "Plot2.png", width = 480, height = 480)
